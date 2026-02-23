2d2270acd5b1c670869f7a5d950d8b8b83f4af89
liumingan <31801071+liumingan@users.noreply.github.com>
2026-02-23 15:14:30 +0800

    Update install_mihomo.sh

--- install_mihomo.sh
+++ install_mihomo.sh
@@ -20,38 +20,73 @@ ONLINE_INSTALL_HS || DOWNLOAD_Nikki_HS
 
 # 启用 Nikki，并进行基本配置
 uci batch <<-EOF
-set nikki.config.enabled='1'
-set nikki.config.mixin='0'
-set nikki.config.fast_reload='1'
-
-# 代理模式配置
-set nikki.proxy.tcp_mode='tproxy'
-set nikki.proxy.udp_mode='tproxy'
-set nikki.proxy.ipv6_proxy='1'
-set nikki.proxy.bypass_china_mainland_ip='1'
-set nikki.proxy.bypass_china_mainland_ip6='1'
-set nikki.proxy.router_proxy='1'
-set nikki.proxy.ipv4_dns_hijack='1'
-set nikki.proxy.ipv6_dns_hijack='1'
+set nikki.'config'.enabled='1'
+set nikki.'config'.start_delay='0'
+set nikki.'config'.scheduled_restart='0'
+set nikki.'config'.cron_expression='0
+set nikki.'config'.test_profile='1'
+set nikki.'config'.core_only='0'
 
 # 订阅配置
-set nikki.subscription.name="${Operators}"
-set nikki.subscription.url="https://raw.githubusercontent.com/liumingan/nikki_ini/main/${Operators}.yaml"
-set nikki.subscription.user_agent='mihomo'
-set nikki.@subscription.prefer='remote'
+set nikki.'subscription'.name="${Operators}"
+set nikki.'subscription'.url="https://raw.githubusercontent.com/liumingan/nikki_ini/main/${Operators}.yaml"
+set nikki.'subscription'.user_agent='mihomo'
+set nikki.'subscription'.prefer='remote'
 
 # Mixin 配置
-set nikki.mixin.dns_respect_rules='0'
 set nikki.mixin.log_level='silent'
-set nikki.mixin.ipv6='1'
-set nikki.mixin.socks_port='7891'
-set nikki.mixin.redir_port='7893'
-set nikki.mixin.tun_stack='mixed'
-set nikki.mixin.dns_mode='redir-host'
-set nikki.mixin.dns_ipv6='1'
-set nikki.mixin.dns_hosts='1'
-set nikki.mixin.dns_system_hosts='1'
-set nikki.mixin.geosite_url='https://ghfast.top/https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat'
+set nikki.'mixin'.mode='rule'
+set nikki.'mixin'.match_process='off'
+set nikki.'mixin'.ipv6='1'
+set nikki.'mixin'.ui_url='https://github.com/Zephyruso/zashboard/releases/latest/download/dist-cdn-fonts.zip'
+set nikki.'mixin'.api_listen='[::]:9090'
+set nikki.'mixin'.selection_cache='1'
+set nikki.'mixin'.allow_lan='1'
+set nikki.'mixin'.http_port='8080'
+set nikki.'mixin'.socks_port='7891'
+set nikki.'mixin'.mixed_port='7890'
+set nikki.'mixin'.redir_port='7893'
+set nikki.'mixin'.tproxy_port='7892'
+set nikki.'mixin'.authentication='1'
+set nikki.'mixin'.tun_device='nikki'
+set nikki.'mixin'.tun_stack='system'
+set nikki.'mixin'.tun_dns_hijack='0'
+add_list nikki.'mixin'.tun_dns_hijacks='tcp://any:53'
+add_list nikki.'mixin'.tun_dns_hijacks='udp://any:53'
+set nikki.'mixin'.dns_listen='[::]:1053'
+set nikki.'mixin'.dns_ipv6='1'
+set nikki.'mixin'.dns_mode='redir-host'
+set nikki.'mixin'.fake_ip_range='198.18.0.1/16'
+set nikki.'mixin'.fake_ip_filter='0'
+add_list nikki.'mixin'.fake_ip_filters='+.lan'
+add_list nikki.'mixin'.fake_ip_filters='+.local'
+set nikki.'mixin'.fake_ip_cache='1'
+set nikki.'mixin'.hosts='0'
+set nikki.'mixin'.dns_nameserver='0'
+set nikki.'mixin'.dns_nameserver_policy='0'
+set nikki.'mixin'.sniffer_force_domain_name='0'
+set nikki.'mixin'.sniffer_ignore_domain_name='0'
+set nikki.'mixin'.sniffer_sniff='0'
+set nikki.'mixin'.rule='0'
+set nikki.'mixin'.rule_provider='0'
+set nikki.'mixin'.mixin_file_content='0'
+set nikki.'mixin'.ui_path='ui'
+set nikki.'mixin'.dns_enabled='1'
+set nikki.'mixin'.tun_enabled='1'
+
+# 代理模式配置
+set nikki.'proxy'.enabled='1'
+set nikki.'proxy'.tcp_mode='tproxy'
+set nikki.'proxy'.udp_mode='tproxy'
+set nikki.'proxy'.ipv4_dns_hijack='1'
+set nikki.'proxy'.ipv6_dns_hijack='1'
+set nikki.'proxy'.ipv4_proxy='1'
+set nikki.'proxy'.ipv6_proxy='1'
+set nikki.'proxy'.fake_ip_ping_hijack='1'
+set nikki.'proxy'.router_proxy='1'
+set nikki.'proxy'.lan_proxy='1'
+set nikki.'proxy'.bypass_china_mainland_ip='1'
+set nikki.'proxy'.bypass_china_mainland_ip6='1'
 
 # 认证
 add nikki authentication
